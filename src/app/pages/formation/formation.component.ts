d59d2d26e841ab6f7a3c9363559089faf66cb81d
Diogo de Paula <depaulatrabalhos@gmail.com>
2022-07-27 14:07:28 -0300

    formação ok

--- src/app/pages/formation/formation.component.ts
+++ src/app/pages/formation/formation.component.ts
@@ -1,23 +1,118 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
+    institution: string;
     name: string;
-    position: number;
-    weight: number;
-    symbol: string;
+
+    date: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+    {
+        institution: 'Alura',
+        name: 'TYPESCRIPT PARTE 3: MAIS TÉCNICAS E BOAS PRÁTICAS',
+        date: '16/09/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'TYPESCRIPT PARTE 2: AVANÇANDO NA LINGUAGEM',
+        date: '08/09/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'TYPESCRIPT PARTE 1: EVOLUINDO SEU JAVASCRIPT',
+        date: '01/09/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'ANGULAR: BOAS PRÁTICAS EM ARQUITETURAS E FORMULÁRIOS',
+        date: '19/09/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'MODELAGEM MONGODB: RELACIONAMENTOS E CARDINALIDADE',
+        date: '04/08/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'MONGODB: MODELAGEM DE DADOS',
+        date: '27/07/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'ANGULAR: EXPLORANDO O FRAMEWORK',
+        date: '29/08/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'MONGODB: UMA ALTERNATIVA AOS BANCOS RELACIONAIS TRADICIONAIS',
+        date: '25/07/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'HTML5 E CSS3 PARTE 4: AVANÇANDO NO CSS',
+        date: '22/05/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'HTML5 E CSS3 PARTE 3: TRABALHANDO COM FORMULÁRIOS E TABELAS',
+        date: '21/05/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'HTML5 E CSS3 PARTE 2: POSICIONAMENTO, LISTAS E NAVEGAÇÃO',
+        date: '21/05/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'SASS: O CSS COM SUPERPODERES',
+        date: '08/05/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'TYPESCRIPT PARTE 1: EVOLUINDO SEU JAVASCRIPT',
+        date: '20/05/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'JAVASCRIPT: CONHECENDO O BROWSER E PADRÕES DE PROJETO',
+        date: '21/08/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'JAVASCRIPT: PROGRAMANDO NA LINGUAGEM DA WEB',
+        date: '07/04/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'JS NA WEB: MANIPULE O DOM COM JAVASCRIPT',
+        date: '21/12/2020',
+    },
+    {
+        institution: 'Alura',
+        name: 'REACT: FUNCTION COMPONENTS, UMA ABORDAGEM MODERNA',
+        date: '02/01/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'HTML5 E CSS3 PARTE 1: CRIE UMA PÁGINA DA WEB',
+        date: '21/05/2021',
+    },
+    {
+        institution: 'Alura',
+        name: 'BOOTSTRAP 4: CRIANDO UMA LANDING PAGE RESPONSIVA',
+        date: '30/09/2020',
+    },
+    {
+        institution: 'Alura',
+        name: 'FLEXBOX: POSICIONE ELEMENTOS NA TELA',
+        date: '26/11/2020',
+    },
+    {
+        institution: 'Alura',
+        name: 'WEB DESIGN RESPONSIVO: PÁGINAS QUE SE ADAPTAM DO MOBILE AO DESK',
+        date: '19/09/2020',
+    },
 ];
 
 @Component({
@@ -26,7 +121,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     styleUrls: ['./formation.component.scss'],
 })
 export class FormationComponent implements OnInit {
-    displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+    displayedColumns: string[] = ['institution', 'name', 'date'];
     dataSource = ELEMENT_DATA;
 
     constructor() {}
