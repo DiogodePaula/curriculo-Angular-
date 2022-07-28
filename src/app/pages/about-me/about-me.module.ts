e343d9e6979e348d87b9dbe9f0f395b24d553606
Diogo de Paula <depaulatrabalhos@gmail.com>
2022-07-28 17:09:33 -0300

    .

--- src/app/pages/about-me/about-me.module.ts
+++ src/app/pages/about-me/about-me.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { AboutMeComponent } from './about-me.component';
 
 @NgModule({
     declarations: [AboutMeComponent],
-    imports: [CommonModule, MatCardModule, FlexLayoutModule, FormsModule],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        FlexLayoutModule,
+        FormsModule,
+        MatDividerModule,
+    ],
     exports: [AboutMeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
